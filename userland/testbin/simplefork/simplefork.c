acf14388107c36b250731187b4acdc232b8bdc20
Federico Bitondo <s276294@studenti.polito.it>
2021-05-19 12:16:38 +0200

    testbin programs

---
+++ userland/testbin/simplefork/simplefork.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int
+main(int argc, char *argv[])
+{
+	pid_t pid;
+	int status;
+
+  (void)argc;
+  (void)argv;
+
+	printf("In the parent BEFORE fork()\n");
+
+	pid = fork();
+
+  printf("fork() returned %d\n", pid);
+
+	switch(pid) {
+	  case -1:
+      printf("Error in fork()\n");
+      return -1;
+    case 0:
+      printf("In child AFTER fork()\n");
+      break;
+    default:
+      printf("In parent AFTER fork(), waiting child with pid = %d\n", pid);
+      waitpid(pid, &status, 0);
+      printf("In parent AFTER fork(), child has exited with status = %d\n", status);
+      break;
+	}
+
+	return 0;
+}
