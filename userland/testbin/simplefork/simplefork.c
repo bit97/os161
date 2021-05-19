273e2da2782d2034326d163ab830dd0f40a3e6d0
Federico Bitondo <s276294@studenti.polito.it>
2021-05-19 18:08:38 +0200

    edit simplefork test

--- userland/testbin/simplefork/simplefork.c
+++ userland/testbin/simplefork/simplefork.c
@@ -1,34 +1,100 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
-int
-main(int argc, char *argv[])
+#define ECHILD 16
+
+static
+void
+fork_test(void)
 {
-	pid_t pid;
-	int status;
+  pid_t pid;
+  int status;
 
-  (void)argc;
-  (void)argv;
-  (void)status;
+  printf("\n\n >>>> fork_test() <<<<<\n\n");
 
-	printf("In the parent BEFORE fork()\n");
+  printf("In the parent BEFORE fork()\n");
 
-	pid = fork();
+  pid = fork();
 
   printf("fork() returned %d, should print twice\n", pid);
 
-	switch(pid) {
-	  case -1:
+  switch(pid) {
+    case -1:
       printf("Error in fork()\n");
-      return -1;
+      return;
     case 0:
       printf("In child AFTER fork()\n");
-      break;
+      _exit(0);
     default:
       printf("In parent AFTER fork(), waiting child with pid = %d\n", pid);
       waitpid(pid, &status, 0);
       printf("In parent AFTER fork(), child has exited with status = %d\n", status);
       break;
-	}
-	return pid;
+  }
+}
+
+static
+void
+waitpid_test(void)
+{
+  int status, res, i;
+  pid_t pid;
+
+  printf("\n\n >>>> waitpid_test() <<<<<\n\n");
+
+  /* Error: no child */
+  res = waitpid(10, &status, 0);
+  if (res < 0) {
+    res = errno;
+    if (res == ECHILD) {
+      printf("Error in waitpid(): ECHILD\n");
+    }
+  }
+
+  pid = getpid();
+  printf("I have pid: %d\n", pid);
+
+  /* Error: self waiting */
+  res = waitpid(pid, &status, 0);
+  if (res < 0) {
+    res = errno;
+    if (res == ECHILD) {
+      printf("Error in waitpid(): ECHILD\n");
+    }
+  }
+
+  for (i = 0; i < 20; i++) {
+    pid = fork();
+
+    if (pid) {
+      // parent
+      res = waitpid(pid, &status, 0);
+      if (res < 0) {
+        res = errno;
+        if (res == ECHILD) {
+          printf("Error in waitpid(): ECHILD\n");
+        }
+      } else {
+        printf("Correctly waited for child with pid: %d\n", pid);
+      }
+    } else {
+      // child
+      printf("exiting from child with pid: %d\n", getpid());
+      _exit(0);
+    }
+  }
+}
+
+int
+main(int argc, char *argv[])
+{
+  (void)argc;
+  (void)argv;
+
+  fork_test();
+  waitpid_test();
+
+	return 0;
 }
